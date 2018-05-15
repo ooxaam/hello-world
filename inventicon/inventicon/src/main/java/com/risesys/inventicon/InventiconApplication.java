557cb748bb8b219f5d6a0837fdcc1942702c59f3
ooxaam <ooxaam@risesys.com>
2018-05-15 15:30:01 +0500

    Project Setup

---
+++ inventicon/inventicon/src/main/java/com/risesys/inventicon/InventiconApplication.java
@@ -0,0 +1,12 @@
+package com.risesys.inventicon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventiconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventiconApplication.class, args);
+	}
+}
